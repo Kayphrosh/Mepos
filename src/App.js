adbf566c433d4cc26336753b48297a70a0e9b673
Uyoyo Whiskey <uyoyowhiskey21@gmail.com>
2024-08-24 13:45:41 +0100

    Implement login screen (first screen)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import Login from "./features/authentication/login/Login";
+
 import Signup from "./features/authentication/signup/Signup";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Signup />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/register-store" element={<Signup />} />
     </Routes>
   );
 }
