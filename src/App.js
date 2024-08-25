a4f5717be6205aa5d3d84e4f1e0ad3a2064823f1
omotayobolu <omotayodaniel10@gmail.com>
2024-08-25 20:55:05 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Signup from "./features/authentication/signup/Signup";
+import RegisterStore from "./features/authentication/register-store/RegisterStore";
+import Login from "./features/authentication/login/Login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/register-store" element={<Signup />} />
+      <Route path="/register-store" element={<RegisterStore />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
